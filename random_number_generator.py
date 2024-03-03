582cb232cf83c22cdfc27e966c58a912029d4a4a
Devansh Bhardwaj <104239369+Devansh1310@users.noreply.github.com>
2024-03-03 16:23:16 -0600

    Add files via upload

---
+++ random_number_generator.py
@@ -0,0 +1,7 @@
+import random
+print()
+print("Random Number Generator")
+print()
+first_value = int(input("From: "))
+second_value = int(input("To: "))
+print(random.randint(first_value, second_value))
